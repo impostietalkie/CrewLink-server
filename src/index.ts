429d459ac4c8c7d8526f8148f131b4d9550d41f1
Carter <cdw77@drexel.edu>
2021-01-30 13:22:11 -0500

    set referer policy

--- src/index.ts
+++ src/index.ts
@@ -49,7 +49,8 @@ app.use(morgan('combined'))
 
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Referrer Policy");
+	res.header("Referrer Policy", "no-referrer");
 	next();
 })
 
